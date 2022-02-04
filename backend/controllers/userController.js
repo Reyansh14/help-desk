454db593dffa986afffd485b9dd778cd78f982ff
Reyansh Patange <reyansh.patange@gmail.com>
2022-02-04 01:11:38 -0500

    set up error and exception handling

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,24 @@
-const registerUser = (req, res) => {
-    res.send('Register Route')
-}
+const asynchandler = require('express-async-handler')
 
-const loginUser = (req, res) => {
+// @desc Register a new user
+// @route /api/users
+// @access Public
+const registerUser = asynchandler(async (req, res) => {
+    const { name, email, password } = req.body
+
+    if (!name || !email || !password) {
+        res.status(400)
+        throw new Error('Include name, email and password fields!');
+    }
+    res.send('Register Route')
+})
+
+// @desc Login a user
+// @route /api/users/login
+// @access Public
+const loginUser = asynchandler(async (req, res) => {
     res.send('Login Route')
-}
+})
 
 module.exports = {
     registerUser,
