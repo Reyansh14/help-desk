2ab596fde1fc454a5ae9f1fdfafb1b9f18c99cf9
Reyansh Patange <reyansh.patange@gmail.com>
2022-02-06 21:47:47 -0500

    created header and routes to home, login, etc.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Header from './components/Header.jsx';
+import Home from './pages/Home.jsx'
+import Login from './pages/Login.jsx'
+import Register from './pages/Register.jsx'
 
 function App() {
-  return (
-    <div>
-      My App
+  return <>
+  <Router>
+    <div className="container">
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+      </Routes>
     </div>
-  );
+  </Router>
+  </>
 }
 
 export default App;
