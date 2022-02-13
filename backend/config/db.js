203eed76d9407d30eddac5188cccfa6fcd01eb64
Reyansh Patange <reyansh.patange@gmail.com>
2022-02-13 12:05:58 -0500

    changes to MONGO_URI

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.mongo_uri)
+        const conn = await mongoose.connect(process.env.MONGO_URI.toString())
         console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
     } catch (error) {
         console.log(`Error: ${error.message}`.red.underline.bold)
